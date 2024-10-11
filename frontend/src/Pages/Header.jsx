18df9a558a225e065a33d231401c528c2ad0eacf
HARSH SHARMA <harsh.sharma.8986@gmail.com>
2024-10-11 10:05:20 +0530

    Added Files

---
+++ frontend/src/Pages/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header(){
+    return (
+        <header>
+                <a href="" className='logo'>Surmise</a>
+                <nav>
+                    <a href=''>Login</a>
+                    <a href=''>Register</a>
+                </nav>
+            </header>
+    )
+}
