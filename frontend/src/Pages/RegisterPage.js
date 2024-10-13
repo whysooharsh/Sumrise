87078e99f74215b3d4a269e6b2ff62d4a10c1808
HARSH SHARMA <harsh.sharma.8986@gmail.com>
2024-10-13 17:41:12 +0530

    Refactor project structure and add new dependencies

--- frontend/src/Pages/RegisterPage.js
+++ frontend/src/Pages/RegisterPage.js
@@ -1,7 +1,18 @@
 import { Form } from "react-router-dom";
 import './LoginPage.css'
+import { useState } from "react";
 
 export default function RegisterPage(){
+    const [Username,setUsername] = useState('');
+    const [Password,setPassword] = useState('');
+    async function register(ev){
+        ev.preventDefault();
+        await fetch('http://localhost:4000/register',{
+            method: 'Post',
+            body: JSON.stringify({Username,Password}),
+
+        })
+    }
     return (
         <div className="container">
         <div className = "heading">
@@ -12,9 +23,17 @@ export default function RegisterPage(){
         Enter your email address to create an account.
         </div>
         <div className="input">
-        <form>
-           <input type="text" placeholder="Username" class="input-field"/>
-           <input type="password" placeholder="Password" class="input-field"/>
+        <form className="register" onSubmit={register}>
+           <input type="text"
+            placeholder="Username"
+            class="input-field"
+            value={Username} 
+            onChange={ev => setUsername(ev.target.value)}/>
+           <input type="password"
+            placeholder="Password"
+            class="input-field"
+            value={Password} 
+            onChange={ev => setPassword(ev.target.value)}/>
            <button>Continue</button>
 
         </form>
