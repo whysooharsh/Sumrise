b1dae8bb366e89b01d7fc02d4ce75fd4f868e565
HARSH SHARMA <harsh.sharma.8986@gmail.com>
2025-02-27 07:38:39 +0530

    build

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+
 import axios from 'axios';
 const API_BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
 axios.defaults.withCredentials = true;
--- frontend/src/Pages/CreatePost.js
+++ frontend/src/Pages/CreatePost.js
@@ -1,3 +1,4 @@
+
 import { useState } from "react";
 import { useNavigate, Navigate } from "react-router-dom";
 import "react-quill/dist/quill.snow.css";
