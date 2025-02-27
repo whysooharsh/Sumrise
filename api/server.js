fed22a7898f90c5a16cf1c290acbd64eb08e80ec
Harsh <116057247+harsSharma45@users.noreply.github.com>
2025-02-27 16:20:16 +0530

    Update server.js

--- api/server.js
+++ api/server.js
@@ -36,7 +36,7 @@ const createPostLimiter = rateLimit({
     legacyHeaders: false,
 });
 
-app.use(limiter); // this might make me sick
+//app.use(limiter); // this might make me sick
 app.use('/api/auth/login', authLimiter); 
 app.use('/api/auth/register', authLimiter);
 app.use('/api/blogs/post', createPostLimiter);
