d51409ce38545e14651e3c06e7bfbea662285d91
harsh <116057247+whysooharsh@users.noreply.github.com>
2025-04-30 07:28:45 +0530

    Update userRoute.js

--- api/Routes/userRoute.js
+++ api/Routes/userRoute.js
@@ -1,12 +1,19 @@
 const { Router } = require("express");
-const router = Router();
-
+const rateLimit = require("express-rate-limit");
 const userController = require('../Controllers/userController');
 const verifyTokenMiddleware = require('../middleware/tokenMiddleware');
 
+const router = Router();
+
+const createUserLimiter = rateLimit({
+    windowMs: 60 * 60 * 1000,
+    max: 20,
+    message: { message: "Too many user creation requests. Try later." }
+});
+
 router.get('/', userController.getAllUsers);
 router.get('/:id', userController.getUserById);
-router.post('/', userController.createUser);
+router.post('/', createUserLimiter, userController.createUser);
 router.put('/:id', verifyTokenMiddleware, userController.updateUser);
 router.delete('/:id', verifyTokenMiddleware, userController.deleteUser);
 router.get('/profile', verifyTokenMiddleware, userController.getUserProfile);
